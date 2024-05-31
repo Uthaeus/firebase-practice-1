2d01d98c281d8aa8648c4c30587922ead198d3e1
Uthaeus <romanlavery@gmail.com>
2024-05-31 13:51:23 -0600

    built initial navigation, root layout, home, login, and register components

---
+++ src/components/layouts/root-layout.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+
+import MainNavigation from "../navigation/main-navigation";
+
+export default function RootLayout() {
+
+    return (
+        <>
+            <MainNavigation />
+            <Outlet />
+        </>
+    );
+}
---
+++ src/components/navigation/main-navigation.js
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+export default function MainNavigation() {
+
+    return (
+        <div className="main-navigation">
+            <NavLink to="/" className={({ isActive }) => isActive ? "nav-link nav-link-active" : "nav-link"}>Home</NavLink>
+            <NavLink to="/login" className={({ isActive }) => isActive ? "nav-link nav-link-active" : "nav-link"}>Login</NavLink>
+            <NavLink to="/register" className={({ isActive }) => isActive ? "nav-link nav-link-active" : "nav-link"}>Register</NavLink>
+        </div>
+    );
+}
